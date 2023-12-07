7e869aa844fa4d3668fd078525d0939ed5ffddf1
Han TaeHui <1cjswosla@gmail.com>
2023-12-07 20:06:25 +0900

    게임 난이도 추가

---
+++ C3_file/C3 Form testing/Form2.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace C3_Form_testing
+{
+    public partial class Form2 : Form
+    {
+        Form1 F1 = new Form1();
+        public int GameCount = 0;
+        private void button1_Click(object sender, MouseEventArgs e)
+        {
+            GameCount = 1;
+            this.Close();
+            F1.Show();
+        }
+        private void button2_Click(object sender, MouseEventArgs e)
+        {
+            GameCount = 2;
+            this.Close();
+        }
+        private void button3_Click(object sender, MouseEventArgs e)
+        {
+            GameCount = 3;
+            this.Close();
+        }
+
+        public Form2()
+        {
+            InitializeComponent();
+        }
+    }
+}
